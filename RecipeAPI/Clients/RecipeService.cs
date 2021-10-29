ed0746d1a16cda5bd1e1f04ff8543bc49c7ef2d6
cairnsi <59540148+cairnsi@users.noreply.github.com>
2021-10-29 14:30:08 -0700

    first commit

---
+++ RecipeAPI/Clients/RecipeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RecipeAPI.Clients
+{
+    public class RecipeService
+    {
+        public HttpClient Client { get; }
+
+        public RecipeService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://api.spoonacular.com");
+
+            Client = client;
+        }
+    }
+}
---
+++ RecipeAPI/Controllers/RecipeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using RecipeAPI.Clients;
+using Newtonsoft.Json.Linq;
+
+namespace RecipeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeController : Controller
+    {
+        HttpClient _recipeClient;
+        public IConfiguration Configuration;
+
+        public RecipeController(RecipeService recipeService, IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+            this._recipeClient = recipeService.Client;
+        }
+        [HttpGet]
+        public async Task<ContentResult> Get()
+        {
+            string path = "/recipes/findByIngredients";
+            string query = "?ingredients=apples,+flour,+sugar&number=2";
+            string auth = $"&apiKey={Configuration["Spoonacular"]}";
+            using var httpResponse = await _recipeClient.GetAsync(path + query + auth);
+
+            httpResponse.EnsureSuccessStatusCode();
+            string content = await httpResponse.Content.ReadAsStringAsync();
+
+            return this.Content(content, "application/json"); ;
+        }
+    }
+}
